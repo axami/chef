87fc7da5c7aa7cfed8f71df2d56fe86ba9c26377
axami <arash.hesami@gmail.com>
2017-06-26 16:37:11 +0100

    create package.rb

---
+++ package.rb
@@ -0,0 +1,15 @@
+if node['platform_family'] == "rhel"
+        package = "httpd"
+elseif node['platform_family'] == "debian"
+        package = "apache2"
+end
+
+package 'apache2' do
+        package_name package
+        action :install
+end
+
+service 'apache2' do
+        service_name = 'httpd'
+        action [:start, :enable]
+end
